0b65f79d54e2f197ca3bfac188178d63fca07e62
Vipransh Upadhyay <115969214+codvi@users.noreply.github.com>
2024-07-06 15:59:48 +0530

    expenses & netEarnings

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,17 @@ const salesRouter = require('./routes/salesRoute.js');
 const livestockRouter = require('./routes/livestockRoutes.js');
 const breedingRoutes = require('./routes/breedingRoutes.js');
 const healthRecordrouter = require('./routes/healthRecordRoutes.js');
+const expenseRouter = require('./routes/expenseRoutes.js');
+const netEarningRoute = require('./routes/netEarning.js');
 
 app.use(farmerRoutes);  
 app.use(porductRouter);
 app.use(salesRouter); 
-app.use(livestockRouter)
-app.use(breedingRoutes)
-app.use(healthRecordrouter)
+app.use(livestockRouter);
+app.use(breedingRoutes);
+app.use(healthRecordrouter);
+app.use(expenseRouter);
+app.use(netEarningRoute);
 app.listen(3000, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
